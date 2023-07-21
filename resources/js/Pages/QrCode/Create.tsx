b9aca3beed2fbea4e992093ad6029a364f656962
Zak <zspratt21@gmail.com>
2023-07-22 02:52:51 +0800

    Centered loading image.

--- resources/js/Pages/QrCode/Create.tsx
+++ resources/js/Pages/QrCode/Create.tsx
@@ -83,7 +83,7 @@ const QrCodeCreate = () => {
     const [errorMessage, setErrorMessage] = useState('There was a problem with the request');
 
     const loading =
-    <div className="min-h-[356px]">
+    <div className="min-h-[356px] flex flex-col justify-center items-center">
         <svg aria-hidden="true"
          className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 sm:m-auto"
          viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
